644b5da256a7addbbbb933389dc89703618693c5
samitfifha <sami.tfifha@esprit.tn>
2021-11-15 20:29:35 +0100

    haja

---
+++ LiveStreamViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LiveStreamViewController.swift
+//  
+//
+//  Created by Apple Esprit on 15/11/2021.
+//
+
+import UIKit
+
+class LiveStreamViewController: UIViewController  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ StandingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StandingViewController.swift
+//  
+//
+//  Created by Apple Esprit on 15/11/2021.
+//
+
+import UIKit
+
+class StandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
